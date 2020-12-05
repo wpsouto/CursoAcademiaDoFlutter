6ec40b4833201c8d48eb4255d55192fc60b39c46
Wemerson Souto <wpsouto@gmail.com>
2020-12-05 18:12:40 -0300

    'refactory'

--- DesafioDartBasico/main.dart
+++ DesafioDartBasico/main.dart
@@ -16,7 +16,7 @@ void main(List<String> args) {
 
   var pacientesList = pacientes.map((e) => e.split('|'));
 
-  print('Quantidade de pacientes com mais de 20 anos : ' +
+  print('Quantidade de Pacientes com mais de 20 anos : ' +
       pacientesList
           .where((element) => int.parse(element[1]) > 20)
           .length
@@ -27,7 +27,6 @@ void main(List<String> args) {
       .map((e) => e[1])
       .toSet()
       .forEach((element) {
-    print('Familia : ${element}');
-    print(pacientesList.map((e) => e[0]).where((e) => e.contains(element)));
+    print('Pacientes da Família ${element} : '+pacientesList.map((e) => e[0]).where((e) => e.contains(element)).toString());
   });
 }
